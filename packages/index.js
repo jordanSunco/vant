f19df7ea43bf65a4a3d2ec555cec0ef8f0544613
jordan <tian8336@163.com>
2018-03-05 10:32:16 +0800

    [build] 0.12.33

--- packages/index.js
+++ packages/index.js
@@ -63,7 +63,7 @@ import TreeSelect from './tree-select';
 import Uploader from './uploader';
 import Waterfall from './waterfall';
 
-const version = '0.12.32';
+const version = '0.12.33';
 const components = [
   Actionsheet,
   AddressEdit,
